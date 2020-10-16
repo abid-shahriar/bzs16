46ac43ab30ca1aec7e904f0c113411a5e51c3a5a
abid7 <abidshahriar7@gmail.com>
2020-10-16 12:39:09 +0600

    Updates

---
+++ precache-manifest.8537dab3f249a508a75e651fb59b4169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756063199ad4b75958053ac93fe02812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0afe7ea3261ce2cc32e",
+    "url": "/static/css/main.2d9e0952.chunk.css"
+  },
+  {
+    "revision": "e10e175d07944a656295",
+    "url": "/static/js/2.36645113.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.36645113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0afe7ea3261ce2cc32e",
+    "url": "/static/js/main.40142c90.chunk.js"
+  },
+  {
+    "revision": "a07f197df8921874f97f",
+    "url": "/static/js/runtime-main.b9fcdf7d.js"
+  },
+  {
+    "revision": "4550543732481b8eda5dab8ca7b1bc54",
+    "url": "/static/media/header.45505437.jpg"
+  }
+]);
