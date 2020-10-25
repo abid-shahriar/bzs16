abe901039ddf678e9c96e565887cf52af43daec9
abid7 <abidshahriar7@gmail.com>
2020-10-26 03:30:27 +0600

    added lazy loading and suspense

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import StudentInfo from "./pages/StudentInfo";
+// import Home from "./pages/Home";
+// import StudentInfo from "./pages/StudentInfo";
 import Header from "./components/Header.component";
 import AllStudentsInfo from "./states/AllStudentsInfo";
 import Footer from "./components/Footer.component";
+
+const Home = lazy(() => import("./pages/Home"));
+const StudentInfo = lazy(() => import("./pages/StudentInfo"));
 
 function App() {
   return (
     <AllStudentsInfo>
       <Header />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/student-info" component={StudentInfo} />
+        <Suspense fallback={<div>loading page...</div>}>
+          <Route exact path="/" component={Home} />
+          <Route path="/student-info" component={StudentInfo} />
+        </Suspense>
       </Switch>
       <Footer />
     </AllStudentsInfo>
